63e3f92da57a1237af7a1af82735dc7664ef538d
Thetruth777 <kianski@rocketmail.com>
2019-12-27 18:48:35 +0100

    Refactor class structure

--- src/editor/toolbar/Toolbar.js
+++ src/editor/toolbar/Toolbar.js
@@ -12,55 +12,19 @@ import '../Editor.css'
 class Toolbar extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {documentName: 'Untitled Document'};
+        this.state = {documentName: null};
         this.documentNameInput = React.createRef();
-    }
-
-    render(){
-        return(
-            <Row>
-                <Col sm>
-                    <ButtonToolbar>
-                        <DropdownButton size="sm" as={ButtonGroup} title="Download as..." id="bg-nested-dropdown" variant="success">
-                            <Dropdown.Item eventKey="1" onClick={() => this.downloadAsMarkdown()}>Markdown</Dropdown.Item>
-                            <Dropdown.Item eventKey="2" onClick={() => this.downloadAsHtml()}>HTML</Dropdown.Item>
-                            <Dropdown.Item eventKey="3">PDF</Dropdown.Item>
-                        </DropdownButton>
-
-                        <Button variant="dark" size="sm" className={'button-style'} >
-                            Save session
-                        </Button>
-
-                        <Button variant="danger" size="sm" className={'button-style'} onClick={() => {this.resetEditor()}}>
-                            Reset document
-                        </Button>
-
-                    </ButtonToolbar>
-
-                </Col>
-                <Col sm>
-                    <Form.Control
-                        type="text"
-                        value={this.state.documentName}
-                        onChange={() => {this.setDocumentName()}}
-                        ref={this.documentNameInput}
-                        className={'document-name'}
-                    />
-                </Col>
-            </Row>
-        );
+        this.defaultDocumentName = 'Untitled Document';
     }
 
     componentDidMount() {
-        const defaultDocumentName = 'Untitled Document';
-
         if (localStorage.getItem("documentName") !== null){
             this.setState({documentName: localStorage.getItem("documentName")});
             return;
         }
 
-        this.setState({documentName: defaultDocumentName});
-        localStorage.setItem('documentName', defaultDocumentName);
+        this.setState({documentName: this.defaultDocumentName});
+        localStorage.setItem('documentName', this.defaultDocumentName);
     }
 
     setDocumentName(){
@@ -97,6 +61,42 @@ class Toolbar extends React.Component{
         this.props.setEditorState('', '');
         this.state.documentName = 'Untitled Document';
         localStorage.removeItem('markdownValue');
+        localStorage.setItem('documentName', this.defaultDocumentName);
+    }
+
+    render(){
+        return(
+            <Row>
+                <Col sm>
+                    <ButtonToolbar>
+                        <DropdownButton size="sm" as={ButtonGroup} title="Download as..." id="bg-nested-dropdown" variant="success">
+                            <Dropdown.Item eventKey="1" onClick={() => this.downloadAsMarkdown()}>Markdown</Dropdown.Item>
+                            <Dropdown.Item eventKey="2" onClick={() => this.downloadAsHtml()}>HTML</Dropdown.Item>
+                            <Dropdown.Item eventKey="3">PDF</Dropdown.Item>
+                        </DropdownButton>
+
+                        <Button variant="dark" size="sm" className={'button-style'} >
+                            Save session
+                        </Button>
+
+                        <Button variant="danger" size="sm" className={'button-style'} onClick={() => {this.resetEditor()}}>
+                            Reset document
+                        </Button>
+
+                    </ButtonToolbar>
+
+                </Col>
+                <Col sm>
+                    <Form.Control
+                        type="text"
+                        value={this.state.documentName}
+                        onChange={() => {this.setDocumentName()}}
+                        ref={this.documentNameInput}
+                        className={'document-name'}
+                    />
+                </Col>
+            </Row>
+        );
     }
 }
 
