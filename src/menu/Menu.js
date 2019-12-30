88955fdf16c3ff80cca80705452db4598c9eb9d9
Thetruth777 <kianski@rocketmail.com>
2019-12-30 17:00:44 +0100

    Add modal for the about the project page

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown, Modal, Row, Col, Container, Button} from 'react-bootstrap';
 
 import './Menu.css';
 
 class Menu extends React.Component{
+
+    callModalAbout() {
+        const [show, setShow] = React.useState(false);
+
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+
+        return (
+            <>
+                <Nav.Link href="#home" onClick={handleShow}>What is Textler.IO</Nav.Link>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>What is Textler.IO</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Textler.IO is a simple markdown editor. It offers you
+                        a direct preview of the markdown you have written.
+                        You can also export your final result as an HTML or
+                        PDF file.
+
+                        If you don't know what the markdown syntax is,
+                        checkout this <a target={'_blank'} href={'https://en.wikipedia.org/wiki/Markdown'}>Wikipedia article here</a>.
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
+
+
+
     render() {
         return(
             <Navbar bg="dark" expand="lg" id={'mainMenu'} variant="dark">
@@ -12,9 +48,9 @@ class Menu extends React.Component{
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">What is Textler.IO</Nav.Link>
+                        <this.callModalAbout />
                         <Nav.Link href="#link">Markdown Overview</Nav.Link>
-                        <Nav.Link href="#github">Fork me on Github</Nav.Link>
+                        <Nav.Link target={'_blank'} href="https://github.com/theTruth777/textler.io">Fork me on Github</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
