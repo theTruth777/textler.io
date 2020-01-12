27be6b1ab8889ee35a5c1aff0b6398502d93b87a
Levent Deniz <LeventDeniz@gmx.de>
2020-01-12 16:26:57 +0100

    Fix editor preview positioning

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -6,8 +6,6 @@ import EditorBody from './body/EditorBody';
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-markdown";
 import "ace-builds/src-noconflict/theme-textmate";
-import 'github-markdown-css/github-markdown.css'
-import 'bootstrap/dist/css/bootstrap.css';
 import './Editor.css';
 
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+/** globally avialable styles */
+import 'github-markdown-css/github-markdown.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
